685af33eed73fcac7d7b0c74d61a5f6457befb59
adam-kurczynski <124350634+adam-kurczynski@users.noreply.github.com>
2025-03-26 16:49:00 +0100

    change fetch method for login

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -58,7 +58,7 @@ async function onSubmit(event) {
   const body = event.data;
   loading.value = true;
   try {
-    await $fetch("/api/auth/login", {
+    await fetch("/api/auth/login", {
       method: "POST",
       body
     })
