99394b2658b68c61df995f0c5153c6040b1ae94a
Adam <ad.kurczynski@gmail.com>
2025-05-19 20:56:16 +0200

    Remove date restrictions from time offs

--- app/components/TimeOffForm.vue
+++ app/components/TimeOffForm.vue
@@ -64,20 +64,6 @@ const onSubmit = async (event: FormSubmitEvent<TimeOffForm>) => {
     });
     return;
   }
-  if (startDate < new Date()) {
-    toast.add({
-      title: 'Data rozpoczęcia nie może być wcześniejsza niż dzisiaj',
-      color: 'error'
-    });
-    return;
-  }
-  if (endDate < new Date()) {
-    toast.add({
-      title: 'Data zakończenia nie może być wcześniejsza niż dzisiaj',
-      color: 'error'
-    });
-    return;
-  }
   try {
     await $fetch('/api/time-off', {
       method: 'POST',
